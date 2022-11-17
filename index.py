b2a8dd851b452dc8dd7504c49cf01501c7c52630
mbuc1c <mbuc1c@users.noreply.github.com>
2022-11-17 14:09:18 +0100

    Post method via forms added

--- index.py
+++ index.py
@@ -1,17 +1,38 @@
-from flask import Flask, make_response, redirect, request
+from flask import Flask, make_response, redirect, request, render_template, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 from webbrowser import open_new_tab
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7a72eaa4821026fa49844c6cb02f30f104bb6c655bf9d1c1'
+
+messages = [{'title': 'Message One', 'content': 'Message One Content'},
+            {'title': 'Message Two', 'content': 'Message Two Content'}]
+
+class LoginForm(FlaskForm):
+    username = StringField('Username: ')
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
-    ipAdresa = request.remote_addr
-    return 'Unesi /user/svojeIme ;) ' + ipAdresa
+    return render_template('index.html', messages=messages)
+    
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
 
-@app.route('/user/<name>')
-def helloUser(name):
-    return f'<h1>Hello {name}!</h1>'
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
 
 @app.route('/makeResponse')
 def makeResponse():
